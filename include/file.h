15eb5cb831136b7a601631eeefef4a1361f839da
qminh743 <qminh3004@gmail.com>
2026-04-04 23:43:02 +0700

    Update file.h

--- include/file.h
+++ include/file.h
@@ -1,12 +1,36 @@
-/*
- * file.h
- *
- * Description:
- * Header file for file.c. Contains function prototypes for file
- * operations used in the program.
- *
- * Responsibilities:
- * - Declare functions for reading/writing files
- * - Provide interface between file.c and other modules
- *
- */
+#ifndef VAR_H
+#define VAR_H
+
+/* ==========================================================
+STRUCTS DEFINITION
+THIS MODULE DEFINE ALL THE DATA STRUCTURE USED IN THE SYSTEM
+========================================================== */
+
+// This is data structure to store the data
+typedef struct {
+    char gameName[100];
+    int game_Id;
+    float price;
+} Game;
+
+typedef struct {
+    char gameName[100];
+    int game_Id;
+    float price;
+} Purchase;
+
+
+// This is data structure to implement linked list and queue
+
+typedef struct GameNode{
+    Game data;
+    struct GameNode *next;
+} GameNode;
+
+typedef struct PurchaseNode{
+    Purchase data;
+    struct PurchaseNode *front;
+    struct PurchaseNode *rear;
+} PurchaseNode;
+
+#endif
