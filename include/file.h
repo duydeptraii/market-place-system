a90cc01e97e4c65cb019ecc420ba8e49387bb0b8
qminh743 <qminh3004@gmail.com>
2026-04-05 17:23:38 +0700

    Update file.h

--- include/file.h
+++ include/file.h
@@ -29,8 +29,56 @@ typedef struct GameNode{
 
 typedef struct PurchaseNode{
     Purchase data;
-    struct PurchaseNode *front;
-    struct PurchaseNode *rear;
+    struct PurchaseNode *next;
 } PurchaseNode;
+typedef struct  {
+    PurchaseNode *front;
+    PurchaseNode *rear;
+} PurchaseQueue;
 
+
+/* ==========================================================
+   QUEUE FUNCTION DECLARATIONS
+   ========================================================== */
+
+// Initialize queue
+void initQueue(PurchaseQueue *q);
+
+// Check if queue is empty
+int isQueueEmpty(PurchaseQueue *q);
+
+// Add element to queue
+void enqueuePurchase(PurchaseQueue *q, Purchase data);
+
+// Remove element from queue
+int dequeuePurchase(PurchaseQueue *q, Purchase *data);
+
+// View front element without removing
+int peekQueue(PurchaseQueue *q, Purchase *data);
+
+// Display all queue elements
+void displayQueue(PurchaseQueue *q);
+
+
+/* ==========================================================
+   LINKED LIST FUNCTIONS FOR GAME
+   ========================================================== */
+
+// Initialize game list
+void initGameList(GameNode **head);
+
+// Add a game to the list
+void insertGame(GameNode **head, Game data);
+
+// Delete a game by ID
+int deleteGame(GameNode **head, int gameId);
+
+// Search game by ID
+GameNode* searchGame(GameNode *head, int gameId);
+
+// Display all games
+void displayGames(GameNode *head);
+
+// Free entire game list
+void freeGameList(GameNode **head);
 #endif
