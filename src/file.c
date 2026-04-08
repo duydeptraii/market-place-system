94e23ab827d93d791325e03093dd94e1ad016050
duydeptraii <duydangxuan91@gmail.com>
2026-04-08 16:50:23 +0700

    ini file.c code

--- src/file.c
+++ src/file.c
@@ -21,3 +21,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../include/file.h"
+#include "../include/logic.h"
+
+saveGamesToFile(GameNode *head) {
+    FILE *f = fopen("data/games.txt","w");
+    if (f == NULL) {
+        printf("Error: can't open games.txt for writing\n");
+        return;
+    }
+
+    GameNode *curr = head;
+    while (curr != NULL){
+        fprintf(f,"%d|%s|%.2f\n",
+        curr->data.gameId,
+        curr->data.gameName,
+        curr->data.price);
+    curr = curr->next;
+    }
+
+    fclose(f);
+    printf("Games saved successfully. \n");
+}
+
+void loadGamesFromFile(GameNode **head) {
+    FILE *f = fopen("data/games.txt", "r");
+    if (f == NULL){
+        printf("no existing game file \n");
+        return;
+    }
+
+    int id;
+    float price;
+    char name[100];
+
+    while (fscanf(f,"%d|%99[^|]%f\n", &id, name, &price) == 3) {
+        Game g;
+        g.gameId =id;
+        strcpy(g.gameName, name);
+        g.price = price;
+        insertGame(head,g);
+    }
+
+    fclose(f);
+    printf("games loaded successfully.\n");
+}
+
+void saveHistoryToFile(PurchaseQueue *q){
+    FILE *f = fopen("data/history.txt", "w");
+    if(f == NULL){
+        printf("error: cannot open history.txt for writing\n");
+        return;
+    }
+
+    PurchaseNode  *curr = q->front;
+    while(curr!=NULL){
+        fprintf(f, "%d|%s|%.2f\n",
+                curr->data.gameId,
+                curr->data.gameName,
+                curr->data.price);
+        curr = curr->next;
+    }
+
+    fclose(f);
+    printf("history saved successfully");
+}
