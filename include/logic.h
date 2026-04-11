f8e15f1d3bbec2f309b40744709aa1a7e57d1aca
duydeptraii <duydangxuan91@gmail.com>
2026-04-12 00:11:38 +0700

    update comments

--- include/logic.h
+++ include/logic.h
@@ -1,17 +1,3 @@
-/*
- * logic.h
- *
- * Description:
- * Header file for logic.c. Contains structure definitions and
- * function prototypes for managing the game list and purchase queue.
- *
- * Responsibilities:
- * - Define struct Game and struct Purchase
- * - Declare all functions used in logic.c
- * - Allow other files (main.c, file.c) to access logic functions
- *
- */
-
 #ifndef LOGIC_H
 #define LOGIC_H
 
